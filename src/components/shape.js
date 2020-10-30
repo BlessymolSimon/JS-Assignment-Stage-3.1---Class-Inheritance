5f5a77feca91ced18b15ef3a91904ddb7b27bb5a
Blessymol Simon <blessy4mooc@gmail.com>
2020-10-31 00:08:01 +0530

    edited as per qn

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,15 @@
 // declare class
+class Shape {
+    constructor() {
+        this.color="red";
+    }
+    drawShape(){
+        console.log("drawing a shape");
+    }
+    calculateArea() {
+        console.log("calculating area of a shape");
+    }
+}
 
 // export class using module.exports
+module.exports=Shape;
